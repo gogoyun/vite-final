79f2ece12cbd27620be26ab61d931e48ea2caf7a
gojohnun <gojohnun@gmail.com>
2024-08-27 22:11:51 +0800

    init commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,73 @@
+<script setup>
+  
+</script>
+<template>
+	<div class="todoList_list">
+		<ul class="todoList_tab">
+			<li><a href="#" class="active">全部</a></li>
+			<li><a href="#">待完成</a></li>
+			<li><a href="#">已完成</a></li>
+		</ul>
+		<div class="todoList_items">
+			<ul class="todoList_item">
+				<li>
+					<label class="todoList_label">
+						<input class="todoList_input" type="checkbox" value="true">
+						<span>把冰箱發霉的檸檬拿去丟</span>
+					</label>
+					<a href="#">
+						<i class="fa fa-times"></i>
+					</a>
+				</li>
+				<li>
+					<label class="todoList_label">
+						<input class="todoList_input" type="checkbox" value="true">
+						<span>打電話叫媽媽匯款給我</span>
+					</label>
+					<a href="#">
+						<i class="fa fa-times"></i>
+					</a>
+				</li>
+				<li>
+					<label class="todoList_label">
+						<input class="todoList_input" type="checkbox" value="true">
+						<span>整理電腦資料夾</span>
+					</label>
+					<a href="#">
+						<i class="fa fa-times"></i>
+					</a>
+				</li>
+				<li>
+					<label class="todoList_label">
+						<input class="todoList_input" type="checkbox" value="true">
+						<span>繳電費水費瓦斯費</span>
+					</label>
+					<a href="#">
+						<i class="fa fa-times"></i>
+					</a>
+				</li>
+				<li>
+					<label class="todoList_label">
+						<input class="todoList_input" type="checkbox" value="true">
+						<span>約vicky禮拜三泡溫泉</span>
+					</label>
+					<a href="#">
+						<i class="fa fa-times"></i>
+					</a>
+				</li>
+				<li>
+					<label class="todoList_label">
+						<input class="todoList_input" type="checkbox" value="true">
+						<span>約ada禮拜四吃晚餐</span>
+					</label>
+					<a href="#">
+						<i class="fa fa-times"></i>
+					</a>
+				</li>
+			</ul>
+			<div class="todoList_statistics">
+				<p> 5 個已完成項目</p>
+			</div>
+		</div>
+	</div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+
+<script setup>
+  import TodoList from '@/components/TodoList.vue'
+</script>
+
+<template>
+  <div id="todoListPage" class="bg-half">
+    <nav>
+      <h1><a href="#">ONLINE TODO LIST</a></h1>
+      <ul>
+        <RouterLink to="/"><li class="todo_sm"><a href="#"><span>王小明的代辦</span></a></li></RouterLink>
+        <RouterLink to="#"><li><a href="#loginPage">登出</a></li></RouterLink>
+      </ul>
+    </nav>
+    <div class="conatiner todoListPage vhContainer">
+      <div class="todoList_Content">
+        <div class="inputBox">
+          <input type="text" placeholder="請輸入待辦事項">
+          <a href="#">
+            <i class="fa fa-plus"></i>
+          </a>
+        </div>
+        <TodoList />
+      </div>
+    </div>
+  </div>
+</template>
